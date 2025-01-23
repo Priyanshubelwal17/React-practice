c88266281a1ad6ff8e61864bd8d063009f6ced86
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-01-24 00:10:17 +0530

    Add unique key prop for list items

--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -2,7 +2,7 @@ export default function ShoppingList({ items }) {
     return (
         <ul>
             {items.map((i) => (
-                <li style={{
+                <li key={i.id} style={{
                     color: i.completed ? "grey" : "red",
                     textDecoration: i.completed ? "line-through" : "none"
                 }}
