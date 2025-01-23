eed7b85df5105827623d776a7abea496e2cf74e4
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-01-23 18:58:29 +0530

    Render colors as styled list

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,17 @@ import Die from './Rolldie.jsx'
 import ListPicker from './Listpicker.jsx'
 import DoubleDice from './DoubleDice.jsx'
 import Heading from './Heading.jsx'
+import ColorList from './ColorList.jsx'
 
 function App() {
   return (
     <div>
-      <Heading color='magenta' text="welcome!" fontSize="90px" />
+      <ColorList colors={["red", "purple", "green", "yellow", "black"]} />
+      {/* <Heading color='magenta' text="welcome!" fontSize="90px" />
       <Heading color='teal' text="blah!" fontSize="40px" />
       <DoubleDice />
       <DoubleDice />
-      <DoubleDice />
+      <DoubleDice /> */}
       {/* <ListPicker values={[1, 2, 4, 3, 5, 6]} /> */}
       {/* <Greeter person='bob' />
       <Die numsides={3} />
