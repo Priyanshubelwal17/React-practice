3c2a84db9e1c21beb557d9e0ec080577fe97d5fb
Priyanshubelwal <142991870+Priyanshubelwal17@users.noreply.github.com>
2025-01-24 13:16:01 +0530

    Split shopping list item into its own component for reusability

--- src/ShoppingList.jsx
+++ src/ShoppingList.jsx
@@ -1,14 +1,15 @@
+import ShoppingListitems from "./ShoppingListitems";
+
 export default function ShoppingList({ items }) {
     return (
         <ul>
             {items.map((i) => (
-                <li key={i.id} style={{
-                    color: i.completed ? "grey" : "red",
-                    textDecoration: i.completed ? "line-through" : "none"
-                }}
-                >
-                    {i.item} - {i.quantity}
-                </li>
+                <ShoppingListitems
+                    key={i.id}
+                    item={i.item}
+                    quantity={i.quantity}
+                    completed={i.completed}
+                />
             ))}
         </ul>
     )
